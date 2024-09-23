c0956a99539925480aaffab337e04dd188b8b4cc
Roman Volosatovs <rvolosatovs@riseup.net>
2024-09-23 21:10:53 +0200

    feat(example-http-server): update `west`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- _examples/http-server/main_test.go
+++ _examples/http-server/main_test.go
@@ -3,21 +3,18 @@
 package main
 
 import (
+	"io"
 	"log"
 	"log/slog"
+	"net/http"
 	"os"
 	"testing"
-	"unsafe"
 
-	"github.com/bytecodealliance/wasm-tools-go/cm"
 	"github.com/rvolosatovs/west"
 	_ "github.com/rvolosatovs/west/bindings"
-	testtypes "github.com/rvolosatovs/west/bindings/wasi/http/types"
-	teststreams "github.com/rvolosatovs/west/bindings/wasi/io/streams"
-	httpext "github.com/rvolosatovs/west/bindings/wasiext/http/ext"
+	"github.com/rvolosatovs/west/westhttp"
 	"github.com/stretchr/testify/assert"
 	incominghandler "go.wasmcloud.dev/component/gen/wasi/http/incoming-handler"
-	"go.wasmcloud.dev/component/gen/wasi/http/types"
 )
 
 func init() {
@@ -34,101 +31,33 @@ func init() {
 
 func TestIncomingHandler(t *testing.T) {
 	west.RunTest(t, func() {
-		headers := testtypes.NewFields()
-		headers.Append(
-			testtypes.FieldKey("foo"),
-			testtypes.FieldValue(cm.NewList(
-				unsafe.SliceData([]byte("bar")),
-				3,
-			)),
-		)
-		headers.Append(
-			testtypes.FieldKey("foo"),
-			testtypes.FieldValue(cm.NewList(
-				unsafe.SliceData([]byte("baz")),
-				3,
-			)),
-		)
-		headers.Set(
-			testtypes.FieldKey("key"),
-			cm.NewList(
-				unsafe.SliceData(
-					[]testtypes.FieldValue{
-						testtypes.FieldValue(cm.NewList(
-							unsafe.SliceData([]byte("value")),
-							5,
-						)),
-					},
-				),
-				1,
-			),
-		)
-		req := testtypes.NewOutgoingRequest(headers)
-		req.SetPathWithQuery(cm.Some("/test"))
-		req.SetMethod(testtypes.MethodGet())
-		out := httpext.NewResponseOutparam()
-		incominghandler.Exports.Handle(
-			types.IncomingRequest(httpext.NewIncomingRequest(req)),
-			types.ResponseOutparam(out.F0),
-		)
-		out.F1.Subscribe().Block()
-		respOptResRes := out.F1.Get()
-		respResRes := respOptResRes.Some()
-		if !assert.NotNil(t, respResRes) || !assert.Nil(t, respResRes.Err()) {
-			t.FailNow()
+		req, err := http.NewRequest("", "/test", nil)
+		if err != nil {
+			t.Fatalf("failed to create new HTTP request: %s", err)
 		}
-		respRes := respResRes.OK()
-		if !assert.Nil(t, respRes.Err()) {
-			t.Fatal(*respRes.Err())
+		req.Header.Add("foo", "bar")
+		req.Header.Add("foo", "baz")
+		req.Header.Add("key", "value")
+		resp, err := westhttp.HandleIncomingRequest(incominghandler.Exports.Handle, req)
+		if err != nil {
+			t.Fatalf("failed to handle incoming HTTP request: %s", err)
 		}
-		resp := respRes.OK()
-		assert.Equal(t, testtypes.StatusCode(200), resp.Status())
-
-		hs := map[string][][]byte{}
-		for _, h := range resp.Headers().Entries().Slice() {
-			k := string(h.F0)
-			hs[k] = append(hs[k], h.F1.Slice())
-		}
-		assert.Equal(t, map[string][][]byte{
+		assert.Equal(t, 200, resp.StatusCode)
+		assert.Equal(t, http.Header{
 			"content-type": {
-				[]byte("text/plain"),
+				"text/plain",
 			},
 			"x-requested-path": {
-				[]byte("/test"),
+				"/test",
 			},
 			"x-existing-paths": {
-				[]byte("/error,/form,/headers,/post"),
+				"/error,/form,/headers,/post",
 			},
-		}, hs)
-		bodyRes := resp.Consume()
-		if !assert.Nil(t, bodyRes.Err()) {
-			t.FailNow()
+		}, resp.Header)
+		buf, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatalf("failed to read HTTP response body: %s", err)
 		}
-
-		body := bodyRes.OK()
-		bodyStreamRes := body.Stream()
-		if !assert.Nil(t, bodyStreamRes.Err()) {
-			t.FailNow()
-		}
-
-		bodyStream := bodyStreamRes.OK()
-		var buf []byte
-		for {
-			bufRes := bodyStream.BlockingRead(4096)
-			if bufRes.IsErr() && *bufRes.Err() == teststreams.StreamErrorClosed() {
-				break
-			}
-			if !assert.Nil(t, bufRes.Err()) {
-				if !assert.False(t, bufRes.Err().Closed()) {
-					t.FailNow()
-				} else {
-					t.Fatal(*bufRes.Err().LastOperationFailed())
-				}
-			}
-			buf = append(buf, bufRes.OK().Slice()...)
-		}
-
 		assert.Equal(t, []byte(Index), buf)
-		bodyStream.ResourceDrop()
 	})
 }
